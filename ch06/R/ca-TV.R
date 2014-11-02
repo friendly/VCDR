738e503eb6f767287ee25652a1c90bbf527535bf
Michael Friendly <friendly@yorku.ca>
2014-11-02 16:20:05 -0500

    add another subsection 6.2.4 to Ch06

---
+++ ch06/R/ca-TV.R
@@ -0,0 +1,37 @@
+library(vcdExtra)
+data("TV", package="vcdExtra")
+
+library(ca)
+
+TV2 <- margin.table(TV, c(1,3))
+TV2
+# this should be an example in ch05, \S 5.3, or done side-by-side with the 2D ca solution
+
+
+pdf(file="TV2-ca.pdf", h=6, w=6)
+op <- par(cex=1.3, mar=c(5,4,1,1)+.1)
+TV.ca <- ca(TV2)
+res <- plot(TV.ca)
+segments(0, 0, res$cols[,1], res$cols[,2], col="red", lwd=2)
+par(op)
+dev.off()
+
+summary(TV.ca)
+
+pdf(file="TV2-mosaic0.pdf", h=6, w=6)
+mosaic(TV2, shade=TRUE, legend=FALSE, labeling=labeling_residuals, suppress=0)
+dev.off()
+
+# better: sort by CA dim 1
+days.order <- order(res$rows[,1]) 
+# same as
+days.order <- order(TV.ca$rowcoord[,1])
+mosaic(TV2[days.order,], shade=TRUE, legend=FALSE, labeling=labeling_residuals, suppress=0)
+
+# transpose to conform to the CA plot
+pdf(file="TV2-mosaic.pdf", h=6, w=6)
+days.order <- order(TV.ca$rowcoord[,1])
+mosaic(t(TV2[days.order,]), shade=TRUE, legend=FALSE, labeling=labeling_residuals, suppress=0)
+dev.off()
+ 
+
---
+++ ch06/R/mca-TV2.R
@@ -0,0 +1,44 @@
+
+library(vcdExtra)
+data("TV", package="vcdExtra")
+
+# reduce number of levels of Time
+TV.df <- as.data.frame.table(TV)
+levels(TV.df$Time) <- rep(c("8:00-8:59", "9:00-9:59", "10:00-10:44"), c(4, 4, 3))
+
+# re-label for  display
+levels(TV.df$Time) <- c("8", "9", "10")
+TV3 <- xtabs(Freq ~ Day + Time + Network, TV.df)
+structable(TV3)
+TV.mca <- mjca(TV3)
+summary(TV.mca)
+
+
+res <- plot(TV.mca, labels=0, pch='.', cex.lab=1.2)
+coords <- data.frame(res$cols)
+faclevs <- strsplit(rownames(coords), ":")
+coords$factor <- sapply(faclevs, function(x) x[1])
+coords$levels <- sapply(faclevs, function(x) x[2])
+coords
+
+cols <- c("red", "green3", "blue")
+nlev <- table(coords$factor)
+segments(0, 0, coords[9:11,1], coords[9:11,2], adjustcolor(col=cols[3], alpha=0.3), lwd=2)
+
+points(coords[,1:2], pch=rep(15:17, nlev), col=rep(cols, nlev), cex=1.25)
+text(coords[,1:2], coords$levels, col=rep(cols,nlev), cex=1.2, pos=3, xpd=TRUE)
+
+legend("topright",
+	legend=unique(coords$factor),
+	title = "Factor", title.col="black", 
+	col=cols, text.col=cols, pch=15:17,
+	bg="gray90", cex=1.2)
+	
+
+
+
+
+
+
+
+
