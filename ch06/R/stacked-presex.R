a71d0b8e885c0660de186181ba28298639d327e4
Michael Friendly <friendly@yorku.ca>
2015-01-03 13:26:14 -0500

    added an exercise to ch06

---
+++ ch06/R/stacked-presex.R
@@ -0,0 +1,23 @@
+data(PreSex, package="vcd")
+library(vcd)
+
+
+stacked <- as.matrix(structable(Gender + Marital ~ Pre + Extra, data=PreSex), sep=":")
+names(dimnames(stacked)) <- c("Pre:Extra", "Gender:Marital")
+str(stacked)
+
+
+
+library(ca)
+stacked.ca <- ca(stacked)
+summary(stacked.ca)
+res <- plot(stacked.ca)
+res
+
+#lines(res$cols[1:2,1], res$cols[1:2,2], col="red")
+#lines(res$cols[3:4,1], res$cols[3:4,2], col="red")
+
+polygon(res$cols[c(1,2,4,3),1], res$cols[c(1,2,4,3),2], border="red")
+
+#polygon(res$rows[c(1,2,4,3),1], res$rows[c(1,2,4,3),2], border="blue")
+
