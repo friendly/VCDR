a169f14155df282501d0c02c9e90f7a24bb90be0
Michael Friendly <friendly@yorku.ca>
2015-01-12 19:09:55 -0500

    added to ChapmanHall/reviews

--- ch06/R/stacked-presex.R
+++ ch06/R/stacked-presex.R
@@ -1,12 +1,10 @@
 data(PreSex, package="vcd")
 library(vcd)
 
-
+# [GM] [PE]
 stacked <- as.matrix(structable(Gender + Marital ~ Pre + Extra, data=PreSex), sep=":")
 names(dimnames(stacked)) <- c("Pre:Extra", "Gender:Marital")
 str(stacked)
-
-
 
 library(ca)
 stacked.ca <- ca(stacked)
@@ -18,6 +16,5 @@ res
 #lines(res$cols[3:4,1], res$cols[3:4,2], col="red")
 
 polygon(res$cols[c(1,2,4,3),1], res$cols[c(1,2,4,3),2], border="red")
-
 #polygon(res$rows[c(1,2,4,3),1], res$rows[c(1,2,4,3),2], border="blue")
 
