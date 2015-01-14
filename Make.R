1300ab3ac6ac4293ec913716e3ec4596e7c47ec5
Michael Friendly <friendly@yorku.ca>
2015-01-14 10:42:10 -0500

    modified Make.R

--- Make.R
+++ Make.R
@@ -1,12 +1,9 @@
-# Attempt at an R makefile for VCDR, modified from Christopher Gandrud's R Series book, Reproducible Research with R and Rstudio
-
 #################
 # Make file for the book
-# Updated: 31 July 2013
+# Updated: 1 Jan 2015
 #################
 
 # This R source code compiles the manuscript for the book.
-# It also updates the main README file.
 
 ## Installing required packages
 # Note: To install the R packages used to compile the book open the Source/FrontMatter/Packages.Rnw. 
@@ -16,25 +13,19 @@
 # Load knitr package
 library(knitr)
 
-# Specify working directories.
+# Specify working directories --- No, not portable.  Assume this is source'd() or run in the cwd for revision1
 
 Main <- 'book.Rnw'
-Repo <- "C:/Dropbox/Documents/VCDR"
-#Source <- paste0(Repo, 'source')
-Source <- Repo
-
-##Setups <- paste0(Repo, 'setups')
-#ParentDirectory <- "/git_repositories/Rep-Res-Book/Source/"
-### README.Rmd
-#SetupDirectory <- "/git_repositories/Rep-Res-Book/Writing_Setup/"
+# Repo <- "C:/Dropbox/Documents/VCDR"
+# Source <- Repo
+# setwd(Source)
 
 ##### Create PDF Book Manuscript ####
-# Compile the book's parent document
-setwd(Source)
-knitr::knit2pdf(input = 'book.Rnw')
+knitr::knit2pdf(input = 'book.Rnw', quiet=TRUE)
 # this also runs bibtex and makeindex via tools::texi2pdf
 
 # may need to run BibTeX again sometimes... followed by pdflatex at the end
+cat("\nRunning BibTex, makeindex, authorindex, aux2bib...\n")
 system("bibtex book")
 # make other indices: example index, author index
 system("makeindex -o book.ine book.ide")
@@ -46,7 +37,7 @@ system("pdflatex book")
 # Embed fonts
 # This is largely for complete replication purposes only and is not necessary.
 ## If using Windows please see extrafont set up instructions at https://github.com/wch/extrafont
-# extrafont::embed_fonts("Rep-Res-Parent.pdf")
+# extrafont::embed_fonts("book.pdf")
 
 # Clean up /git_repositories/Rep-Res-Book/Source/Parent/
 # DeleteFiles <- setdiff(list.files(source), c("Rep-Res-Parent.Rnw", 
@@ -56,9 +47,3 @@ system("pdflatex book")
 #                       )
 # unlink(DeleteFiles)
 
-##### Update README ####
-## Change working directory to /Rep-Res-Book/Writing_Setup/
-#setwd(Setups)
-#
-## Knit README file
-#knitr::knit(input = "README.Rmd", output = paste0(Repo, "README.md")
