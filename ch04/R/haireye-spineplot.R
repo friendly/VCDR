eb61dd3cd984bf5bc36a6711a3f7e526f7194b2e
Michael Friendly <friendly@yorku.ca>
2015-01-18 12:46:17 -0500

    modified .gitignore; recompiled ch03

---
+++ ch04/R/haireye-spineplot.R
@@ -0,0 +1,17 @@
+# barplots, spineplots and tile plots
+
+# what's wrong with barplots?
+HE <- margin.table(HairEyeColor, 2:1)  # as in Table 4.2
+barplot(HE, xlab="Hair color", ylab="Frequency")
+#barplot(t(HE), xlab="Eye color", ylab="Frequency")
+
+# why spineplots are better
+#spineplot(HE)
+spineplot(t(HE))
+
+# 
+tile(HE)
+tile(HE, tile_type="width")
+tile(HE, tile_type="height")
+
+
