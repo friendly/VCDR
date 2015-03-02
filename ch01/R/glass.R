87fff2d16ec90d0c695a9b8fa65bc9c384a37e22
Michael Friendly <friendly@yorku.ca>
2015-03-02 12:49:33 -0500

    modified ch01/R/glass.R and Fig 1.10 figures

--- ch01/R/glass.R
+++ ch01/R/glass.R
@@ -1,3 +1,9 @@
+# Demonstrate effect ordering
+
+# fixed: use rot_labels to avoid overlapping text labels.
+
+# TODO: add Glass data to vcdExtra
+
 cd("c:/sasuser/catdata")
 Glass <- read.csv("glass.csv", stringsAsFactors=FALSE)
 
@@ -13,10 +19,10 @@ gargs <- list(interpolate=c(1,2,4,8))
 
 # unordered
 
-cd("C:/Documents and Settings/friendly/My Documents/My Dropbox/Documents/VCDR/ch01/fig")
+cd("C:/Dropbox/Documents/VCDR/ch01/fig")
 pdf(file="glass-mosaic1.pdf", h=6, w=6)
 mosaic(glass.tab, shade=TRUE, labeling_args=largs, gp_args=gargs,
-  main="Alphabetic order", legend=FALSE)
+  main="Alphabetic order", legend=FALSE, rot_labels=c(20,90,0,70))
 dev.off()
 
 
@@ -24,11 +30,11 @@ dev.off()
 ord <- c(2, 1, 4, 3, 5) 
 pdf(file="glass-mosaic2.pdf", h=6, w=6)
 mosaic(glass.tab[ord, ord], shade=TRUE, labeling_args=largs,  gp_args=gargs,
-  main="Effect order", legend=FALSE)
+  main="Effect order", legend=FALSE, rot_labels=c(20,90,0,70))
 dev.off()
 
+# makes a good exercise in Ch 6 
 library(ca)
-
 plot(ca(glass.tab))
 
 
--- ch03/R/arbuthnot.R
+++ ch03/R/arbuthnot.R
@@ -1,5 +1,33 @@
 data(Arbuthnot, package="HistData")
 
+
+# plot Pr(Male)
+op <- par(mar=c(4,4,1,1)+.1, cex.lab=1.25)
+data(Arbuthnot, package = "HistData")
+with(Arbuthnot, {
+  prob = Males / (Males + Females)
+  plot(x = Year, y = prob, type = "b", 
+       ylim = c(0.5, 0.54), ylab = "Pr (Male)")
+  abline(h = 0.5, col = "red", lwd = 2)
+  abline(h = mean(prob), col = "blue")
+  lines(loess.smooth(Year, prob), col = "blue", lwd = 2)
+  text(x = 1640, y = 0.5, expression(H[0]: "Pr(Male)=0.5"), pos = 3, col = "red")
+  })
+par(op)
+
+# using scatter.smooth()
+op <- par(mar=c(4,4,1,1)+.1, cex.lab=1.25)
+prob <- with(Arbuthnot, Males / (Males + Females))
+scatter.smooth(x = Arbuthnot$Year, y = prob, type = "b", 
+               lpars = list(col = "blue", lwd = 2),
+               xlab = "Year", ylab = "Pr(Male)", ylim = c(0.5, 0.54))
+abline(h = 0.5, col = "red", lwd = 2)
+abline(h = mean(prob), col = "blue")
+text(x = 1640, y = 0.5, expression(H[0]: "Pr(Male)=0.5"), pos = 3, col = "red")
+par(op)
+
+
+
 # plot the sex ratios
 with(Arbuthnot, plot(Year,Ratio, type='b', ylim=c(1, 1.20), ylab="Sex Ratio (M/F)"))
 abline(h=1, col="red", lwd=2)
@@ -8,17 +36,4 @@ text(x=1630, y=1, expression(H[0]), pos=3, col="red")
 #  add loess smooth
 Arb.smooth <- with(Arbuthnot, loess.smooth(Year,Ratio))
 lines(Arb.smooth$x, Arb.smooth$y, col="blue", lwd=2)
-
-
-# plot Pr(Male)
-with(Arbuthnot, {
-  prob = Males/(Males+Females)
-	plot(Year, prob, type='b', ylim=c(0.5, 0.54), ylab="Pr (Male)")
-  abline(h=0.5, col="red", lwd=2)
-	abline(h=mean(prob), col="blue")
-	text(x=1640, y=0.5, expression(H[0]: "Pr(Male)=0.5"), pos=3, col="red")
-	Arb.smooth <- loess.smooth(Year,prob)
-	lines(Arb.smooth$x, Arb.smooth$y, col="blue", lwd=2)
-	}
-	)
 
