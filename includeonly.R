72d446035397ee8cfb680d8ce3be51c10b0e833f
Michael Friendly <friendly@yorku.ca>
2015-03-06 16:49:18 -0500

    removed Further reading sections

--- includeonly.R
+++
@@ -1,9 +0,0 @@
-chapters <- sprintf("ch%02d", 1:12)
-includeonly = c("ch01", "ch02", "ch03")
-
-includes = function(file) {
-  if (file %in% includeonly) paste0(file, '.Rnw')
-}
-
-## use as:
-##<<ch1, child=includes('ch01')>>=
