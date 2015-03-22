272a00abd1ebae3667d75361f62935f5114bf2e6
David Meyer <david.meyer@technikum-wien.at>
2015-03-23 00:28:05 +0100

    adapted to new vcd version

--- ch07/R/binregplot.R
+++ ch07/R/binregplot.R
@@ -19,10 +19,11 @@ grid.lines(arth.loess$x, arth.loess$y,
 # FIXME -- xlim should be honored in predicted values in binreg_plot: the goal is to show
 # prediction for low temperatures.  The ggplot version allows fullrange=TRUE
 # that accomplishes this.
+# DM: fixed
 shuttle.mod <- glm(cbind(nFailures, 6 - nFailures) ~ Temperature,
                    data = SpaceShuttle, na.action = na.exclude, family = binomial)
-binreg_plot(shuttle.mod, xlim=c(30, 81),
-ylab = "O-Ring Failure Probability", xlab = "Temperature (F)")
+binreg_plot(shuttle.mod, xlim = c(30, 81), pred_range = "xlim",
+            ylab = "O-Ring Failure Probability", xlab = "Temperature (F)")
 
 ## Figure 7.6, 7.7
 ## Conditional plots are out of scope of binreg_plot, designed for
@@ -34,9 +35,9 @@ ylab = "O-Ring Failure Probability", xlab = "Temperature (F)")
 arth.logistic2 <- glm(Better ~ I(Age - 50) + Treatment,
                       data = Arthritis, family = binomial)
 binreg_plot(arth.logistic2, type = "link", subset = Sex == "Female",
-            main = "Female", xlim=c(25, 75), ylim = c(-2, 2))
+            main = "Female", xlim = c(25, 75), ylim = c(-2, 2))
 binreg_plot(arth.logistic2, type = "link", subset = Sex == "Male",
-            main = "Male", xlim=c(25, 75), ylim = c(-2, 2))
+            main = "Male", xlim = c(25, 75), ylim = c(-2, 2))
 
 ## Figure 7.9
 # -- make xlim the same
@@ -46,15 +47,15 @@ binreg_plot(arth.logistic2, subset = Sex == "Male", main = "Male",
             xlim=c(25, 75))
 
 
-## Figure 7.15 (as a full-model plot!)
+## ## Figure 7.15 (as a full-model plot!)
 
-data("Donner", package="vcdExtra")
-donner1 <- glm(survived ~ age + sex, data = Donner)
-binreg_plot(donner1)
+## data("Donner", package="vcdExtra")
+## donner1 <- glm(survived ~ age + sex, data = Donner)
+## binreg_plot(donner1)
 
-## Figure 7.16, left (as a full-model plot!)
-donner2 <- glm(survived ~ poly(age, 2) + sex, data = Donner)
-binreg_plot(donner2)
+## ## Figure 7.16, left (as a full-model plot!)
+## donner2 <- glm(survived ~ poly(age, 2) + sex, data = Donner)
+## binreg_plot(donner2)
 
 ## Figure 7.23
 data("ICU", package="vcdExtra")
