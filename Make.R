6e9f39291e62483f75c9beadcf3bc3e315cd22f5
Michael Friendly <friendly@yorku.ca>
2015-05-22 11:43:41 -0400

    now include book.log for use with texdepend and Makefile

--- Make.R
+++ Make.R
@@ -1,24 +1,14 @@
 #################
 # Make file for the book
-# Updated: 1 Jan 2015
+# Updated: 22 May 2015
 #################
 
 # This R source code compiles the manuscript for the book.
 
-## Installing required packages
-# Note: To install the R packages used to compile the book open the Source/FrontMatter/Packages.Rnw. 
-# Find: doInstall <- FALSE in the code chunk labeled "FrontPackageCitations". 
-# Change the value `FALSE` to `TRUE` and run the code chunk.
-
 # Load knitr package
 library(knitr)
 
-# Specify working directories --- No, not portable.  Assume this is source'd() or run in the cwd for revision1
-
 Main <- 'book.Rnw'
-# Repo <- "C:/Dropbox/Documents/VCDR"
-# Source <- Repo
-# setwd(Source)
 
 ##### Create PDF Book Manuscript ####
 knitr::knit2pdf(input = 'book.Rnw', quiet=TRUE)
@@ -30,7 +20,9 @@ system("bibtex book")
 # make other indices: author index
 system("perl authorindex -d book")
 # create references.bib from separate bib files under local texmf tree
+# -- no longer required, now that any changes in references.bib are being hand-edited
 system("perl aux2bib book")
+
 system("pdflatex book")
 
 # Embed fonts
@@ -38,7 +30,7 @@ system("pdflatex book")
 # sizes, so this should make them happier. embed_fonts() also makes the file size smaller.
 
 ## If using Windows, see extrafont set up instructions at https://github.com/wch/extrafont
-# It requires to set the path to GS, so this setting is not portable.
+# It requires to set the path to GS, or else GS must be on the path, so this setting is not portable.
 
 Sys.setenv(R_GSCMD = "C:/Program Files/gs/gs9.14/bin/gswin64c.exe")
 extrafont::embed_fonts("book.pdf")
