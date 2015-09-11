5512c06b7131d6742912f222507c3e5c0ee82db2
Michael Friendly <friendly@yorku.ca>
2015-09-11 15:58:09 -0400

    kludge to fix issue #53 re car::linearHypothesis

---
+++ ch11/R/nmes-vglm-test.R
@@ -0,0 +1,22 @@
+data("NMES1988", package="AER")
+nmes2 <- NMES1988[, c(1:4, 6:8, 13, 15, 18)]
+str(nmes2)
+
+# vector generalized additive model
+library(VGAM)
+nmes2.nbin   <- vglm(cbind(visits, nvisits, ovisits, novisits) ~ ., data = nmes2, 
+family = negbinomial)
+summary(nmes2.nbin)
+# one term
+lh <- paste("hospital:", 1:3, " = ", "hospital:", 2:4, sep="")
+lh
+
+# John Fox's kludge:
+
+	df.residual.vglm <- function(object, ...) object@df.residual
+	vcov.vglm <- function(object, ...) vcovvlm(object, ...)
+	coef.vglm <- function(object, ...) coefvlm(object, ...)
+
+car::linearHypothesis(nmes2.nbin, lh)
+
+
