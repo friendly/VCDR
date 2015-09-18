f19df94bc25cc3aa8c760819eb564dd02b15f20d
Michael Friendly <friendly@yorku.ca>
2015-09-18 11:39:49 -0400

    update ch11/R/nmes-vglm-test.R

--- ch11/R/nmes-vglm-test.R
+++ ch11/R/nmes-vglm-test.R
@@ -12,13 +12,30 @@ lh <- paste("hospital:", 1:3, " = ", "hospital:", 2:4, sep="")
 lh
 
 # John Fox's kludge:
+library(car)
 
 if (packageVersion("car") < "2.1.1") {
 	df.residual.vglm <- function(object, ...) object@df.residual
 	vcov.vglm <- function(object, ...) vcovvlm(object, ...)
 	coef.vglm <- function(object, ...) coefvlm(object, ...)
 }
+linearHypothesis(nmes2.nbin, lh)
 
-car::linearHypothesis(nmes2.nbin, lh)
+## John's extension to make Anova() work
+
+Anova(nmes2.nbin)
+
+linearHypothesis(nmes2.nbin, matchCoefs(nmes2.nbin, "hospital")) # check
+
+# model with interaction
+
+mod2 <- vglm(cbind(visits, nvisits, ovisits, novisits) ~ hospital*health, 
+data=nmes2, family=negbinomial)
+
+Anova(mod2)
+
+linearHypothesis(mod2, matchCoefs(mod2, "hospital:health")) # check
+
+
 
 
