b3b385c69314f838710deadf680083ab406ebbe1
Michael Friendly <friendly@yorku.ca>
2015-11-18 16:22:12 -0500

    makeRcode.R -> extract R code from chapters

---
+++ makeRcode.R
@@ -0,0 +1,11 @@
+# extract R code from chapters
+
+outdir <- "Rcode/"
+chapters <- c(sprintf("ch%02d", 1:11))
+
+for (ch in chapters) {
+  input <- paste0(ch, ".Rnw")
+  output <- paste0(outdir, ch, ".R")
+  message(paste("\n**purling", input, "->", output))
+  knitr::purl(input, output=output, quiet=TRUE)
+}
